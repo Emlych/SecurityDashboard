f25b34cffc263848a626d7a9d159eace9cf17e49
Emily LD <lagreouemily@gmail.com>
2023-12-25 20:38:08 +0100

    Add structure for services, dashboard components and widgets

---
+++ src/app/security-dashboard/dashboard-widgets/dashboard-widgets.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-widgets',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-widgets.component.html',
+  styleUrl: './dashboard-widgets.component.scss'
+})
+export class DashboardWidgetsComponent {
+
+}
---
+++ src/app/security-dashboard/security-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-security-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './security-dashboard.component.html',
+  styleUrl: './security-dashboard.component.scss'
+})
+export class SecurityDashboardComponent {
+
+}
